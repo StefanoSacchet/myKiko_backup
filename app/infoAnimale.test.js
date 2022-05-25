7ae129d9a06a896f61ea8ec6e392edd878d6959f
Stefano <sacchet13@gmail.com>
2022-05-25 17:28:23 +0200

    modifiche

---
+++ app/infoAnimale.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const app = require('./app');
+const mongoose = require('mongoose');
+ 
+describe('GET /api/v1/infoAnimale', () => {
+
+    let connection;
+
+    beforeAll( async () => {
+        jest.setTimeout(8000);
+        jest.unmock('mongoose');
+        connection = await  mongoose.connect(process.env.DB_URL);
+        console.log('Database connected!');
+        //return connection; // Need to return the Promise db connection?
+    });
+    
+    afterAll( () => {
+        mongoose.connection.close(true);
+        console.log("Database connection closed");
+    });
+
+    test('GET /api/v1/infoAnimale with email not specified', () => {
+        return request(app)
+          .get('/api/v1/infoAnimale')
+          .expect('Content-Type', /json/)
+          .expect( { success: false, message: 'Authentication failed. User not found.' } );
+    });
+
+    let email = "prova1";    
+    test('GET /api/v1/infoAnimale with email specified', async () => {
+        expect.assertions(1);
+        const response = await request(app).get('/api/v1/infoAnimale?email='+ email);
+        expect(response.statusCode).toBe(200);
+    });
+});
---
+++ app/registration.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const app = require('./app');
+const mongoose = require('mongoose');
+
+describe('POST /api/v1/registration', () => {
+
+    let connection;
+
+    beforeAll( async () => {
+        jest.setTimeout(8000);
+        jest.unmock('mongoose');
+        connection = await  mongoose.connect(process.env.DB_URL);
+        console.log('Database connected!');
+        //return connection; // Need to return the Promise db connection?
+    });
+    
+    afterAll( () => {
+        mongoose.connection.close(true);
+        console.log("Database connection closed");
+    });
+
+    test('POST /api/v1/registration with an empty input', () => {
+        return request(app)
+          .post('/api/v1/registration')
+          .send({ email: '', password: '' }) // sends a JSON post body
+          .expect('Content-Type', /json/)
+          .expect(400, { success: false, message: 'Registration failed. input empty' } );
+    });
+
+    test('POST /api/v1/registration with existing user', () => {
+        return request(app)
+          .post('/api/v1/registration')
+          .send({ email: 'prova2', password: '2' }) // sends a JSON post body
+          .expect('Content-Type', /json/)
+          .expect( { success: false, message: 'Registration failed. User already subscribed.' } );
+    });
+
+    /*test('POST /api/v1/registration with new user', () => { //Aggiunge al db user tmp
+        return request(app)
+          .post('/api/v1/registration')
+          .send({ email: 'tmp', password: 'tmp' }) // sends a JSON post body
+          .expect('Content-Type', /json/)
+          .expect(201, { success: true, message: 'User subscribed', self: 'api/v1/undefined' } );
+    });*/
+});
