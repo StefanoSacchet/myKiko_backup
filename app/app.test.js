51b4e23c6c3bf07419e1c84448cea0b9f8a7416e
Stefano <sacchet13@gmail.com>
2022-05-25 14:17:36 +0200

    modifiche

--- app/app.test.js
+++ app/app.test.js
@@ -1,7 +1,6 @@
-
 const request = require('supertest');
-const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tokens
-const app     = require('./app');
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const app = require('./app');
  
 test('app module should be defined', () => {
   expect(app).toBeDefined();
