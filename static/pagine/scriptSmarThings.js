e68251bf3ce0bf4efee29bbca5db44f2ccef4e14
Simone Molinari <simonemolinari01@gmail.com>
2022-05-27 17:29:08 +0200

    schermata smart

--- static/pagine/scriptSmarThings.js
+++ static/pagine/scriptSmarThings.js
@@ -1,6 +1,16 @@
 var email = sessionStorage.getItem("email");
 
 
+
+
+
+
+
+
+function aggiungiDispositivo(){
+    console.log("Ciao Ste");
+}
+
 //Go back to home screen
 function goBack(){
     document.location.href = '/home.html';
