d968487521aa222040b29d4eaabdf26c954d0088
Stefano <sacchet13@gmail.com>
2022-05-27 22:18:09 +0200

    modifiche

---
+++ app/models/animali.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const User = require('./models/user');// get our mongoose model
+
+router.post('/aggiungiAnimale', async function(req,res){
+
+    let userEmail = req.body.email;
+    let nomeNew = req.body.nomeNew;
+    let razzaNew = req.body.razzaNew;
+    let etaNew = req.body.etaNew;
+    let pesoNew = req.body.pesoNew;
+    let codiceChipNew = req.body.codiceChipNew;
+
+    if(nomeNew == "" || razzaNew == "" || etaNew == "" || pesoNew == "" || codiceChipNew == ""){
+        res.status(400).json({ success: false, message: 'Empty inputs' })
+    }else{
+
+        //Find the user
+	    let user = await User.findOne({
+		    email: userEmail
+	    }).exec();
+
+        if(!user){
+            res.json( { success: false, message: 'User not found' } )
+        }else{
+
+            user = await User.updateOne( { email: userEmail }, { $push: { "animale": {nome: nomeNew, razza: razzaNew, eta: etaNew, peso: pesoNew, codiceChip: codiceChipNew} } });
+
+            //console.log(user);
+    
+            if(user.acknowledged == true){
+                res.status(201).json({ success: true, message: 'Data inserted' })
+            }else{
+                res.json({ success: false, message: 'Error. add failed' })
+            }
+        }
+    }
+});
+
+module.exports = router;
